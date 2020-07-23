0d937784b8dbd3f4f68f8b05e6bbb3050a7df569
rqai52 <109814927+rqai52@users.noreply.github.com>
2020-07-23 07:17:01 +0000

    102

--- app/controllers/api/coins_controller.rb
+++ app/controllers/api/coins_controller.rb
@@ -2,3 +2,7 @@ class Api::CoinsController < ApplicationController
 
   before_action :set_coin, only: [:update, :destroy]
   BASE_URL = 'https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest?CMC'
+
+  def index
+    listings = HTTParty.get("#{BASE_URL}")['data'].values
+    user_coins = c
