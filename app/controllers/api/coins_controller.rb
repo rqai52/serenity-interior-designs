b6abcce4392de8208b465f2720188dfacf65c397
rqai52 <109814927+rqai52@users.noreply.github.com>
2020-07-23 12:54:35 +0000

    Add sponsor

--- app/controllers/api/coins_controller.rb
+++ app/controllers/api/coins_controller.rb
@@ -7,4 +7,5 @@ class Api::CoinsController < ApplicationController
     listings = HTTParty.get("#{BASE_URL}")['data'].values
     user_coins = current_user.coins
     user_coins.each do |coin|
-      res_coin = listings.find 
+      res_coin = listings.find { |c| c['id'] === coin.cmc_id }
+      coin.update(price: res_coin['quote
