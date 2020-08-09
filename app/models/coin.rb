f7244d085462faac1fc34d1651ba448ed8c68796
rqai52 <109814927+rqai52@users.noreply.github.com>
2020-08-09 20:41:32 +0000

    removing stuff

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -2,4 +2,5 @@ class Coin < ApplicationRecord
     validates_uniquieness_of :cmc_id, :name, :symbol, { case_sensitive: false }
     validates_presence_of :cmc_id, :name, :symbol
 
-    has_many :watched_co
+    has_many :watched_coins, dependent :destroy
+    has_many :users, through: :watched_coi
