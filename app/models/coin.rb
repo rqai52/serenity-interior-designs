b9c0d6986a78d8aab90aac7a2b9c189d2ac09965
rqai52 <109814927+rqai52@users.noreply.github.com>
2020-08-09 09:57:28 +0000

    cpuset: Add allow_attach hook for cpusets on android.

--- app/models/coin.rb
+++ app/models/coin.rb
@@ -1,2 +1,5 @@
 class Coin < ApplicationRecord
     validates_uniquieness_of :cmc_id, :name, :symbol, { case_sensitive: false }
+    validates_presence_of :cmc_id, :name, :symbol
+
+    has_many :watched_co
