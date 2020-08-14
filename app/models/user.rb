413bc33c22b65e31f0c4405a8f0109075f2610a6
rqai52 <109814927+rqai52@users.noreply.github.com>
2020-08-14 12:43:16 +0000

    intelli_plug: use mp_decision to reduce online persistence count for cores

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@
 
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
-  # :lockable, :timeoutable and :omniau
+  # :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :rec
