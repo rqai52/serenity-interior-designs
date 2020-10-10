19a8fb0682401b9d39e91c61505187cd5758641c
rqai52 <109814927+rqai52@users.noreply.github.com>
2020-10-10 13:32:44 +0000

    Bump version number

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -3,3 +3,6 @@ import { Switch, Route } from 'react-router-dom';
 import NoMatch from './NoMatch';
 import NavBar from './NavBar';
 import Login from './Login';
+import Register from './Register';
+import Flash from './Flash';
+impor
