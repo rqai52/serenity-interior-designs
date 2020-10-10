f07745112121b5e61f80ea2757e511beb9eb4be7
rqai52 <109814927+rqai52@users.noreply.github.com>
2020-10-10 10:48:05 +0000

    Simplify conditional

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,3 +1,5 @@
 import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
-import NoMatch fr
+import NoMatch from './NoMatch';
+import NavBar from './NavBar';
+import Login from './Login';
