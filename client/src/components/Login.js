997a2d1a6cef24a2fef8bab9f000bc4415e8978f
rqai52 <109814927+rqai52@users.noreply.github.com>
2020-11-03 14:24:44 +0000

    SCSI: bnx2i: Fixed NULL ptr deference for 1G bnx2 Linux iSCSI offload

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,2 +1,4 @@
 import React, { Component } from 'react';
-import { Header, Segment, Form, Button } from 'semantic-ui-
+import { Header, Segment, Form, Button } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { handleLog
