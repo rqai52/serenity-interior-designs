087bd60c57513aab10c9e8129edb7807b7ef7aa7
rqai52 <109814927+rqai52@users.noreply.github.com>
2020-11-08 11:17:32 +0000

    fixed code style

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
 import { Header, Segment, Form, Button } from 'semantic-ui-react';
 import { connect } from 'react-redux';
-import { handleLog
+import { handleLogin } from '../reducers/user';
+
+class Login extends Component {
+  state = { email:
