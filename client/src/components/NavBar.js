7f1043c6a641d53b1f07548782a265bda823a778
rqai52 <109814927+rqai52@users.noreply.github.com>
2020-11-22 21:33:23 +0000

    Testing push

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
 import { Menu } from 'semantic-ui-react';
 import { Link, withRouter } from 'react-router-dom';
-import { conn
+import { connect } from 'react-redux';
+import { handleLogout } from '../reducers/user';
+
+clas
