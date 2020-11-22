8a50c2adfcfefc4e9dfc8e9424cb30081ff255cd
rqai52 <109814927+rqai52@users.noreply.github.com>
2020-11-22 20:12:44 +0000

    usb: option: add support for Telit LE910

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,2 +1,4 @@
 import React, { Component } from 'react';
-import { Menu } from 'semantic-ui-
+import { Menu } from 'semantic-ui-react';
+import { Link, withRouter } from 'react-router-dom';
+import { conn
