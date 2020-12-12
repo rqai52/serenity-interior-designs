f49b89719fb153a40e1c5386e7b3960db96fd5af
rqai52 <109814927+rqai52@users.noreply.github.com>
2020-12-12 06:44:15 +0000

    zram: Add LZ4 support

--- client/src/components/NoMatch.js
+++ client/src/components/NoMatch.js
@@ -1,3 +1,6 @@
 import React, { Component } from 'react';
 import { Header } from 'semantic-ui-react';
-import { Link
+import { Link } from 'react-router-dom';
+
+class NoMatch extends Component {
+  rend
