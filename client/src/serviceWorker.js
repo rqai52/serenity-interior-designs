b6bbadc58b1ff411207263c8c655b4da78ee0ad3
rqai52 <109814927+rqai52@users.noreply.github.com>
2020-12-24 08:07:55 +0000

    source code

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -2,4 +2,5 @@
 // register() is not called by default.
 
 // This lets the app load faster on subsequent visits in production, and gives
-// it offline capabilities.
+// it offline capabilities. However, it also means that developers (and users)
+// will only s
