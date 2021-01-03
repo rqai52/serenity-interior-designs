97482c35fd650fd8250ba0a255accb573f42b7de
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-01-03 09:11:12 +0000

    New translations en.json (Japanese) [ci skip] Signed-off-by: Björn Kimminich <bjoern.kimminich@gmx.de>

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -14,4 +14,6 @@ const isLocalhost = Boolean(
   window.location.hostname === 'localhost' ||
     // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
-    // 127.0.0.1/8 is consider
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|
