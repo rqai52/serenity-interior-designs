940ed2ffe68236f97bed1591e10f8da85c7886c4
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-01-03 09:44:40 +0000

    Add CONTRIBUTING.md

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -16,4 +16,9 @@ const isLocalhost = Boolean(
     window.location.hostname === '[::1]' ||
     // 127.0.0.1/8 is considered localhost for IPv4.
     window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+export function register(config) {
+  if (pro
