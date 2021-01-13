0a2eb0332e86940d997b61507eb8ef09c68d34ce
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-01-13 18:24:36 +0000

    drm/radeon: force dma32 to fix regression rs4xx,rs6xx,rs740

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -24,4 +24,5 @@ export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
-    if (publicU
+    if (publicUrl.origin !== window.location.origin) {
+      // Our service worker won
