a0a61260db7dcd3b50113ea53c0b4807385cd295
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-01-13 16:05:22 +0000

    U.

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -23,4 +23,5 @@ const isLocalhost = Boolean(
 export function register(config) {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
-    const publ
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    if (publicU
