d4900a2f173cc4f88673bb4148206e2aa8f2fdb9
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-01-14 13:49:59 +0000

    Update colors

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -26,4 +26,5 @@ export function register(config) {
     const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
     if (publicUrl.origin !== window.location.origin) {
       // Our service worker won't work if PUBLIC_URL is on a different origin
-      // from what our page is se
+      // from what our page is served on. This might happen if a CDN is used to
+      // serve assets; s
