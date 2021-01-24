857322ae89d942034f10e7899760505be5d270b6
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-01-24 09:59:02 +0000

    add travis ci badge

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -35,4 +35,7 @@ export function register(config) {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
 
       if (isLocalhost) {
-        // This is running on localhost. Let's check if a service worker still 
+        // This is running on localhost. Let's check if a service worker still exists or not.
+        checkValidServiceWorker(swUrl, config);
+
+        /
