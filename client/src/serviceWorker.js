f6360b4490f499952e4b0ea66a96ba30e522119b
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-01-29 14:42:08 +0000

    Whitespace fixes.

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -42,4 +42,5 @@ export function register(config) {
         // service worker/PWA documentation.
         navigator.serviceWorker.ready.then(() => {
           console.log(
-            'This web app is 
+            'This web app is being served cache-first by a service ' +
+              'work
