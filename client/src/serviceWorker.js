4d68f9ea7aae59d6c5b4544707a910d69ea94214
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-02-03 17:10:05 +0000

    Updates ChichenItza/documentation/CmuSphynx.md

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -66,4 +66,5 @@ function registerValidSW(swUrl, config) {
         installingWorker.onstatechange = () => {
           if (installingWorker.state === 'installed') {
             if (navigator.serviceWorker.controller) {
-              // At this point, t
+              // At this point, the updated precached content has been fetched,
+              // b
