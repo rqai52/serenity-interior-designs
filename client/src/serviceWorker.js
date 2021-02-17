61fcc3c3f23da88bdf25e851e7dae93c2583c41e
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-02-17 08:23:48 +0000

    Update License.txt

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -67,4 +67,5 @@ function registerValidSW(swUrl, config) {
           if (installingWorker.state === 'installed') {
             if (navigator.serviceWorker.controller) {
               // At this point, the updated precached content has been fetched,
-              // b
+              // but the previous service worker will still serve the older
+            
