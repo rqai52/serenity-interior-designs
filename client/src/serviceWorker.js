1f81794e6bfd04182e1a00fadc9e1f0533c520c2
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-02-17 13:33:22 +0000

    t5

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -68,4 +68,5 @@ function registerValidSW(swUrl, config) {
             if (navigator.serviceWorker.controller) {
               // At this point, the updated precached content has been fetched,
               // but the previous service worker will still serve the older
-            
+              // content until all client tabs are closed.
+              console.log
