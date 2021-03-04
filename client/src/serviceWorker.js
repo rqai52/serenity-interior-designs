9cbf37baf6df1291297ec1505802ce4a6f57408f
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-03-04 18:03:44 +0000

    Added install.sh

--- client/src/serviceWorker.js
+++ client/src/serviceWorker.js
@@ -102,4 +102,6 @@ function checkValidServiceWorker(swUrl, config) {
   // Check if the service worker can be found. If it can't reload the page.
   fetch(swUrl)
     .then(response => {
-      // Ensure service worker exists, and that we really are getting a JS
+      // Ensure service worker exists, and that we really are getting a JS file.
+      const contentType = response.headers.get('content-type');
+     
