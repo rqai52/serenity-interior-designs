c2474e994fec7efb03754955d671f96dfa31137c
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-03-19 14:34:35 +0000

    chore: eslint fix

--- client/src/store.js
+++ client/src/store.js
@@ -1,3 +1,7 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/index';
+
+const enhancers = compose(
+  applyMiddleware(thunk),
+  window.devToolsExtensi
