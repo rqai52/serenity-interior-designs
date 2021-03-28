f64e3d3d9ce06dc28c6f9ee48b7837500385d7c5
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-03-28 07:04:01 +0000

    Fix clang warnings.

--- client/src/store.js
+++ client/src/store.js
@@ -13,4 +13,8 @@ if (module.hot) {
   module.hot.accept('./reducers/', () => {
     const nextRootReducer = require('./reducers/index').default;
     store.replaceReducer(nextRootReducer);
- 
+  });
+}
+
+export default store;
+
