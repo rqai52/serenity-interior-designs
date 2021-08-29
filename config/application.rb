bc7acb843f8c86dca668bd975530b5d2ff9404c4
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-08-29 15:16:32 +0000

    Add some logging.

--- config/application.rb
+++ config/application.rb
@@ -2,4 +2,6 @@ require_relative 'boot'
 
 require "rails"
 # Pick the frameworks you want:
-require "active_mod
+require "active_model/railtie"
+require "active_job/railtie"
+require "active_record/railtie"
