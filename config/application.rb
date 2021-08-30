c0ab1c8e249bd5fd3110f9dd62c533d14e098798
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-08-30 14:10:50 +0000

    Delete search.html

--- config/application.rb
+++ config/application.rb
@@ -15,4 +15,6 @@ require "action_cable/engine"
 
 
 
-# Require the gems listed in Gemfile, incl
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.re
