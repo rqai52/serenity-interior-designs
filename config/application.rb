c6843efc5ff031c2a679ae378e72c2a019bb4980
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-08-30 06:10:24 +0000

    5.5

--- config/application.rb
+++ config/application.rb
@@ -7,4 +7,6 @@ require "active_job/railtie"
 require "active_record/railtie"
 require "active_storage/engine"
 require "action_controller/railtie"
-requi
+require "action_mailer/railtie"
+require "action_view/railtie"
+require "action_cabl
