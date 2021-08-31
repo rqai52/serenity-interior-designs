03f258270734541cad1e2c82305d70f68a7c0f4f
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-08-31 08:18:09 +0000

    bump to 0.2.3

--- config/application.rb
+++ config/application.rb
@@ -17,4 +17,7 @@ require "action_cable/engine"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
-Bundler.re
+Bundler.require(*Rails.groups)
+
+module Cryptic
+  class Application < Rails::Appli
