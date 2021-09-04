b50bbdb7d4dcc62df1a6eb370fc7a1cac1569ab6
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-09-04 11:49:07 +0000

    update changes for release

--- config/application.rb
+++ config/application.rb
@@ -22,4 +22,6 @@ Bundler.require(*Rails.groups)
 module Cryptic
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-   
+    config.load_defaults 5.2
+
+    # Settings in config/environments/* take prece
