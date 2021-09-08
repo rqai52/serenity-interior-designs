acb80a586e81813d0efee6ce39609db835f83118
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-09-08 06:43:20 +0000

    save state

--- config/application.rb
+++ config/application.rb
@@ -27,4 +27,6 @@ module Cryptic
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
-    # the framework and an
+    # the framework and any gems in your application.
+
+    # Only loads a smaller set of mi
