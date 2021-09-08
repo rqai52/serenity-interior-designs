8e696500de9ea704eeb68161cee68bcbab91fb99
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-09-08 06:12:48 +0000

    msm: camera: sensor: Validate step_boundary

--- config/application.rb
+++ config/application.rb
@@ -26,4 +26,5 @@ module Cryptic
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
-    # -- all .rb files in that d
+    # -- all .rb files in that directory are automatically loaded after loading
+    # the framework and an
