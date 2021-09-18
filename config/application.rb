ca681142fafce069f106a235e3f2ff136233d959
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-09-18 19:36:37 +0000

    Changed favicon

--- config/application.rb
+++ config/application.rb
@@ -35,4 +35,6 @@ module Cryptic
     config.api_only = true
         # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
-    # Skip views, helpers and asse
+    # Skip views, helpers and assets when generating a new resource.
+    config.middleware.use ActionDispatch::Flash
+ 
