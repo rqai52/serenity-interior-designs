9d5f65070b754ea50c2c8dbc0d087a6fb509dc80
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-12-04 22:12:22 +0000

    update repo url

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -2,4 +2,7 @@ require "#{Rails.root}/lib/listen"
 include Listen
 Rails.application.configure do
   config.action_mailer.default_url_options = { host: "localhost: 3001" } 
-  # Settings specified here will take precedence over those in config/applicat
+  # Settings specified here will take precedence over those in config/application.rb.
+
+  # In the development environment your application's code is reloaded on
+  # e
