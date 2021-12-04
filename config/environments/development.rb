83bcbafcc5176ffde6d3e2ed49a1eb3130ff76b6
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-12-04 19:50:19 +0000

    update fonts

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,4 +1,5 @@
 require "#{Rails.root}/lib/listen"
 include Listen
 Rails.application.configure do
-  config.action_mailer.default_url_options = { host: "localh
+  config.action_mailer.default_url_options = { host: "localhost: 3001" } 
+  # Settings specified here will take precedence over those in config/applicat
