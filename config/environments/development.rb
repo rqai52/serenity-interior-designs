81652b86f0ebd887daeeafe812425fc15a5b441f
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-12-06 14:12:29 +0000

    cvs update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -13,4 +13,7 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports.
-  config.consider_all_requ
+  config.consider_all_requests_local = true
+
+  # Enable/disable caching. By default caching is disabled.
+  #
