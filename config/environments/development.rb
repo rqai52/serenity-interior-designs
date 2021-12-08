d373e7059ac24c3f4586d6688f81458c65230c53
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-12-08 08:23:08 +0000

    Updates versions.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -21,4 +21,5 @@ Rails.application.configure do
     config.action_controller.perform_caching = true
 
     config.cache_store = :memory_store
-    config.public_file_server.he
+    config.public_file_server.headers = {
+      'Cache-Control' => "public, max-age=#{2.days.to_
