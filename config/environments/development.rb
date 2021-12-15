496c08e8d6651dc752d2276c9b904fd97c0a4197
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-12-15 07:57:19 +0000

    Reduce code

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -38,4 +38,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
-  # Print deprecation notices to the Rails logge
+  # Print deprecation notices to the Rails logger.
+  config.active_support.deprecation = :log
+
+  # Raise an error on page load if
