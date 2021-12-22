7cea6eafdf644c507b07443c8e87e860fa4ac8ef
rqai52 <109814927+rqai52@users.noreply.github.com>
2021-12-22 12:19:11 +0000

    delete .gitignore

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -50,3 +50,5 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Use an evented file watcher to asynchronously detect
