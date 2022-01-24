5c49d431143f35e497589d62b342632eaf4604d9
rqai52 <109814927+rqai52@users.noreply.github.com>
2022-01-24 17:37:26 +0000

    Conclusion

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -38,4 +38,5 @@ Rails.application.configure do
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
 
-  # Print dep
+  # Print deprecation notices to the stderr.
+  config.active_support.deprecation = :
