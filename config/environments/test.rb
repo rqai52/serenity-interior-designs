1d9a8d2d25b8ead1d8a90f7121dec5be3728229d
rqai52 <109814927+rqai52@users.noreply.github.com>
2022-01-24 13:20:38 +0000

    mm: fix corner case in anon_vma endless growing prevention

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -35,4 +35,7 @@ Rails.application.configure do
 
   # Tell Action Mailer not to deliver emails to the real world.
   # The :test delivery method accumulates sent emails in the
-  # ActionMailer::Base.deliveri
+  # ActionMailer::Base.deliveries array.
+  config.action_mailer.delivery_method = :test
+
+  # Print dep
