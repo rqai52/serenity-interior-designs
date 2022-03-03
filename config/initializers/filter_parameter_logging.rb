1140e423582a9dc125719f866d40719999c7ad19
rqai52 <109814927+rqai52@users.noreply.github.com>
2022-03-03 12:33:08 +0000

    Update legend-atlantis.html

--- config/initializers/filter_parameter_logging.rb
+++ config/initializers/filter_parameter_logging.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
-# Configure sensitive parameters which wil
+# Configure sensitive parameters which will be filtered from the log file.
+Rails.application.config.filter_
