ec1878f342f0f4e60197f16d86c3b47ba9ae19ee
rqai52 <109814927+rqai52@users.noreply.github.com>
2022-10-22 20:58:55 +0000

    block: fiops add ioprio support

--- db/migrate/20181211204822_devise_token_auth_create_users.rb
+++ db/migrate/20181211204822_devise_token_auth_create_users.rb
@@ -7,4 +7,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
       t.string :uid, :null => false, :default => ""
 
       ## Database authenticatable
-      t.string :encrypted_password, :null => false, :default =>
+      t.string :encrypted_password, :null => false, :default => ""
+
+      ## Recoverable
+      t.string   :reset_password_token
