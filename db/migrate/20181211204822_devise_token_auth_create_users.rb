441ff9c54027175f6f23d91c0b13a81bbe7e1a14
rqai52 <109814927+rqai52@users.noreply.github.com>
2022-10-22 18:06:48 +0000

    chore(package): update babel-cli to version 6.18.0

--- db/migrate/20181211204822_devise_token_auth_create_users.rb
+++ db/migrate/20181211204822_devise_token_auth_create_users.rb
@@ -3,4 +3,7 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
     
     create_table(:users) do |t|
       ## Required
-      t.string :provider, :null => false, :defa
+      t.string :provider, :null => false, :default => "email"
+      t.string :uid, :null => false, :default => ""
+
+      ## Database 
