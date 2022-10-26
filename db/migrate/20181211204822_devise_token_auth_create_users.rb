e4bc942d2474d474e1bad7bba0daf763e1164629
rqai52 <109814927+rqai52@users.noreply.github.com>
2022-10-26 11:42:09 +0000

    master updated

--- db/migrate/20181211204822_devise_token_auth_create_users.rb
+++ db/migrate/20181211204822_devise_token_auth_create_users.rb
@@ -20,4 +20,5 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
       ## Trackable
       t.integer  :sign_in_count, :default => 0, :null => false
       t.datetime :current_sign_in_at
-      t.d
+      t.datetime :last_sign_in_at
+      t.string   :current_sign_in_ip
