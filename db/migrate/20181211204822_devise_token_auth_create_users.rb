75b44dc2b9dad3cea3013ee82b267a8737369aa7
rqai52 <109814927+rqai52@users.noreply.github.com>
2022-11-09 11:50:18 +0000

    Release 7.0.1

--- db/migrate/20181211204822_devise_token_auth_create_users.rb
+++ db/migrate/20181211204822_devise_token_auth_create_users.rb
@@ -34,3 +34,8 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
       # t.integer  :failed_attempts, :default => 0, :null => false # Only if lock strategy is :failed_attempts
       # t.string   :unlock_token # Only if unlock strategy is :email or :both
       # t.datetime :locked_at
+
+      ## User Info
+      t.string :name
+      t.string :nickname
+      t.stri
