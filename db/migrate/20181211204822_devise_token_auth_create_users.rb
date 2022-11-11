eb57dafc84b0753b4eb437a2d0b71f8a8c0a85ba
rqai52 <109814927+rqai52@users.noreply.github.com>
2022-11-11 10:54:27 +0000

    add var

--- db/migrate/20181211204822_devise_token_auth_create_users.rb
+++ db/migrate/20181211204822_devise_token_auth_create_users.rb
@@ -49,4 +49,5 @@ class DeviseTokenAuthCreateUsers < ActiveRecord::Migration[5.2]
 
     add_index :users, :email,                unique: true
     add_index :users, [:uid, :provider],     unique: true
-    ad
+    add_index :users, :reset_password_token, unique: true
+    add
