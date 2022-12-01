bb5e3132871423d416cd81d09208efe39451d692
rqai52 <109814927+rqai52@users.noreply.github.com>
2022-12-01 20:21:02 +0000

    Updated project structure

--- db/schema.rb
+++ db/schema.rb
@@ -12,4 +12,8 @@
 
 ActiveRecord::Schema.define(version: 2018_12_11_231903) do
 
-  # These are extensions that must be enabled in order to support this 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "coins", force: :cascade do |t|
+    t.i
