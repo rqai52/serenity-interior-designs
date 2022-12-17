9044260ea9366d9bd9dd031877e52aa39865f88c
rqai52 <109814927+rqai52@users.noreply.github.com>
2022-12-17 16:27:06 +0000

    C

--- db/schema.rb
+++ db/schema.rb
@@ -35,4 +35,6 @@ ActiveRecord::Schema.define(version: 2018_12_11_231903) do
     t.datetime "remember_created_at"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
-  
+    t.datetime "last_sign_in_at"
+    t.string "current_sign_in_ip"
+    t.str
