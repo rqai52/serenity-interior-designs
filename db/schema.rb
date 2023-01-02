3147f1b9298bdfa428a20abef94794287be941f9
rqai52 <109814927+rqai52@users.noreply.github.com>
2023-01-02 17:17:35 +0000

    Improved search

--- db/schema.rb
+++ db/schema.rb
@@ -52,4 +52,7 @@ ActiveRecord::Schema.define(version: 2018_12_11_231903) do
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-    t.index ["uid", "pr
+    t.index ["uid", "provider"], name: "index_users_on_uid_and_provider", unique: true
+  end
+
+  cre
