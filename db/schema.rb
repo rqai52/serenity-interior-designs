8844fba4c3aced23430118e360e3171e0841f26f
rqai52 <109814927+rqai52@users.noreply.github.com>
2023-01-02 06:14:39 +0000

    fix menu

--- db/schema.rb
+++ db/schema.rb
@@ -50,4 +50,5 @@ ActiveRecord::Schema.define(version: 2018_12_11_231903) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
-    t.index ["email"], name: "index_users_on_email", 
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_
