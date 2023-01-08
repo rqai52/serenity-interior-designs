eb87d9c8725dc66691bb6031797e19c59e897f9b
rqai52 <109814927+rqai52@users.noreply.github.com>
2023-01-08 17:13:30 +0000

    Latest Updates

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,2 @@
 # This file should contain all the record creation needed to seed the database with its default values.
-# The dat
+# The data can then be loaded with the rails db:seed command (or created
