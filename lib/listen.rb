291d89b06e1f1b0f05e6c1c9be1a89e47b0a552a
rqai52 <109814927+rqai52@users.noreply.github.com>
2023-01-19 15:32:19 +0000

    Update from Forestry.io Zoltán Bába updated deploy.md

--- lib/listen.rb
+++ lib/listen.rb
@@ -2,4 +2,8 @@ module Listen
   class Record
     class SymlinkDetector
       def _fail(_, _)
-        fail Error, "Don't watch
+        fail Error, "Don't watch locally-symlinked directory twice"
+      end
+    end
+  end
+end
